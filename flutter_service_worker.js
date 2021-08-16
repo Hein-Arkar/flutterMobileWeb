acd77a7221573aa45f0d888bd9d34b8dee41a521
Hein Arkar <arkar131429@gmail.com>
2021-08-16 19:28:41 +0630

    add new animation

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,17 +3,19 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "index.html": "58fdab5081d17da7476ccf15459900b6",
-"/": "58fdab5081d17da7476ccf15459900b6",
+  "index.html": "a2c14bdf1c8cea9ef88939b5c6f8a2a4",
+"/": "a2c14bdf1c8cea9ef88939b5c6f8a2a4",
 "version.json": "a605bbf304581bbe009914be4c368898",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"main.dart.js": "35a906f899f2de1d50c215520824173b",
+"main.dart.js": "dd880faec36851d7c07ec913d5c49459",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/assets/user4.png": "b16451ef79be872aa3317c0f841e018d",
 "assets/assets/user3.png": "1e531c2abb1a3cbe52ba3110bc9b38a0",
+"assets/assets/images/the_kid_laroi.jpg": "2187d2224808f12c7883ec897476170e",
+"assets/assets/images/vinyldisc.png": "78b659ef62955856900947354a80866c",
 "assets/assets/sea.jpg": "fef88612dfe3d4ffbd3d90781a33af7f",
 "assets/assets/sea2.jpg": "204b3acae8016c235d147b34a9681234",
 "assets/assets/mountain.jpg": "2c1809292a000c0ffb36c5282cd9a08a",
@@ -22,7 +24,7 @@ const RESOURCES = {
 "assets/assets/user1.png": "da2c3b1a32ffa664a44e610fc618631d",
 "assets/assets/user2.png": "22e356af4204a35c78bb7f8a05be8c5e",
 "assets/NOTICES": "e424d04f8089aa98ded641512c1c7d15",
-"assets/AssetManifest.json": "0debdcf587dabd8d64ff4350acca6393",
+"assets/AssetManifest.json": "44f2abccd06957b0bfeb2ca2e359c6c7",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
 "manifest.json": "fcbd657cec02a95ca608486f47597da4"
 };
